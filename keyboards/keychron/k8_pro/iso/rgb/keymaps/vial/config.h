fab58d5778334eed6e3eccc07bcac09c125c3e4b
Adophoxia <andyao1528@gmail.com>
2023-04-17 22:16:11 -0700

    Add `vial` keymaps to ISO variants of K8 Pro

---
+++ keyboards/keychron/k8_pro/iso/rgb/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+//Vial Keyboard UID
+#define VIAL_KEYBOARD_UID {0xF5, 0xA0, 0x25, 0x0A, 0x53, 0xB4, 0xE7, 0x25}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
---
+++ keyboards/keychron/k8_pro/iso/white/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+//Vial Keyboard UID
+#define VIAL_KEYBOARD_UID {0x42, 0xC9, 0x5A, 0x75, 0x9A, 0xE1, 0x6B, 0xC9}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
