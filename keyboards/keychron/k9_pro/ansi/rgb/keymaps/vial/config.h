568c6db8dc633ca8576172dcf278a2c367b94d05
Adophoxia <andyao1528@gmail.com>
2023-05-02 18:32:15 -0700

    Add `vial` keymap for K9 Pro

---
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+//Vial Keyboard UID
+#define VIAL_KEYBOARD_UID {0x6C, 0x5C, 0x35, 0x03, 0x8A, 0xBF, 0xCA, 0x55}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
