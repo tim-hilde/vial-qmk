bddb0f9fd0778ac881306d7c1e6032d7378702e5
Adophoxia <andyao1528@gmail.com>
2023-05-02 18:16:42 -0700

    Add `vial` keymap for K13_Pro

---
+++ keyboards/keychron/k13_pro/ansi/rgb/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+//Vial Keyboard UID
+#define VIAL_KEYBOARD_UID {0x27, 0x88, 0xFA, 0xE5, 0x71, 0xF6, 0x29, 0x58}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
---
+++ keyboards/keychron/k13_pro/ansi/white/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+//Vial Keyboard UID
+#define VIAL_KEYBOARD_UID {0xC5, 0xC1, 0x04, 0x69, 0xE8, 0x3C, 0xFF, 0x22}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
