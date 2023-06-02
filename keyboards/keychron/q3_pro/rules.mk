8718fce51d323c7cb96bc7adcc543915ba37d906
Adophoxia <andyao1528@gmail.com>
2023-06-02 12:03:03 -0700

    Add back old emulated eeprom options

--- keyboards/keychron/q3_pro/rules.mk
+++ keyboards/keychron/q3_pro/rules.mk
@@ -12,6 +12,8 @@ DIP_SWITCH_ENABLE = yes
 BLUETOOTH_ENABLE = no
 BLUETOOTH_DRIVER = custom
 RAW_ENABLE = yes
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = embedded_flash
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
