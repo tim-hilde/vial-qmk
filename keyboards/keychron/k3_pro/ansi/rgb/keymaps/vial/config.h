bc66065e022ec5a943e85fe49b2826295719ace5
Jalen J. Dear <39681015+jSQrD-dev@users.noreply.github.com>
2023-06-05 17:03:28 +0000

    Added Vial Support for K3 Pro ANSI (#2)
    * Added Vial Support for K3 Pro ANSI
    
    * Fixing Pull Request
    
    ---------
    
    Co-authored-by: Jalen J. Dear <39681015+J-SQrD-Dev@users.noreply.github.com>

---
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID \
+    { 0xDB, 0xCF, 0xB8, 0x5C, 0xCC, 0x1A, 0x74, 0x7B }
+
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 0, 3 }
+
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 0, 13 }
---
+++ keyboards/keychron/k3_pro/ansi/white/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID \
+    { 0x60, 0xC5, 0xB1, 0xE4, 0x61, 0x9D, 0xBE, 0x9D }
+
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 0, 3 }
+
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 0, 13 }
