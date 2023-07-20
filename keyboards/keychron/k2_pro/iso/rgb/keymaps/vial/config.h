70d3f5798b18045df2362d20e15969b1b01c1d96
tim-hilde <tim.hildebrandt@me.com>
2023-07-20 13:05:33 +0200

    Add k2_pro rgb iso support

---
+++ keyboards/keychron/k2_pro/iso/rgb/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+//Vial Keyboard UID
+#define VIAL_KEYBOARD_UID {0x54, 0xEC, 0x5D, 0x63, 0xAB, 0x57, 0x93, 0xBB}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
